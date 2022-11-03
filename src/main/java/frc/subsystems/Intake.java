c8d5a5d9023cc23c1bd397d2e1c94f5269479b15
Zach W <zwheeler1026@gmail.com>
2022-11-03 16:27:49 -0400

    lol max poop

--- src/main/java/frc/subsystems/Intake.java
+++ src/main/java/frc/subsystems/Intake.java
@@ -1,5 +1,12 @@
 package frc.subsystems;
 
-public class Intake {
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+public class Intake extends SubsystemBase{
     
 }
