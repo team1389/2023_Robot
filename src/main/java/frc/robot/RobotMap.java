3659fd416ab3fcf4cdd00933fc88a6549a1441ff
Zach W <zwheeler1026@gmail.com>
2022-11-03 15:41:19 -0400

    max dumb

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,5 +4,65 @@ package frc.robot;
  * define Hardware Ports in here
  */
 public class RobotMap {
+    //Pneumatics Hub
+    public static final int PNEUMATICS_HUB= 2;
+
+    //Drive motors
+    public static final  int DRIVE_MOTOR_FRONT_LEFT = 3;
+    public static final int DRIVE_MOTOR_FRONT_RIGHT = 5;
+    public static final int DRIVE_MOTOR_BACK_LEFT = 7;
+    public static final int DRIVE_MOTOR_BACK_RIGHT = 9;
+
+    //Rotation motors
+    public static final int ROTATION_MOTOR_FRONT_LEFT = 4;
+    public static final int ROTATION_MOTOR_FRONT_RIGHT = 6;
+    public static final int ROTATION_MOTOR_BACK_LEFT = 8;
+    public static final int ROTATION_MOTOR_BACK_RIGHT = 10;
+
+    //Encoders
+    public static final int DRIVE_ENCODER_FRONT_LEFT = 11;
+    public static final int DRIVE_ENCODER_FRONT_RIGHT = 12;
+    public static final int DRIVE_ENCODER_BACK_LEFT = 13;
+    public static final int DRIVE_ENCODER_BACK_RIGHT = 14;
+
+    //Shooter motor
+    public static final int SHOOTER_MOTOR = 19;
+
+    //Intake motors
+    public static final int INTAKE_MOTOR = 15;
+
+    //Climb motor
+    public static final int RIGHT_CLIMB_MOTOR = 21;
+    public static final int LEFT_CLIMB_MOTOR = 20;
+
+
+    //Turret Motor
+    public static final int TURRET_MOTOR = 17;
+
+    //Indexer Motor
+    public static final int INDEXER_MOTOR = 16;
+
+    //Hopper Motor
+    public static final int HOPPER_MOTOR = 18;
+
+    //Intake Double Solenoids
+    public static final int LEFT_INTAKE_FORWARD_SOLENOID = 7;
+    public static final int LEFT_INTAKE_REVERSE_SOLENOID = 8;
+    public static final int RIGHT_INTAKE_FORWARD_SOLENOID = 5;
+    public static final int RIGHT_INTAKE_REVERSE_SOLENOID = 10;
+
+    //Climber Double Solenoids
+    public static final int LEFT_CLIMBER_FORWARD_SOLENOID = 4;
+    public static final int LEFT_CLIMBER_REVERSE_SOLENOID = 11;
+    public static final int RIGHT_CLIMBER_FORWARD_SOLENOID = 9;
+    public static final int RIGHT_CLIMBER_REVERSE_SOLENOID = 6;
+    
+
+
+
+    //Robot dimensions (inches)
+    //Length and width are measured from the centers of the wheels
+    public static final int L = 29;
+    public static final int W = 29;
 
 }
