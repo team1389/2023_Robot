eeb602650a018145b7ccb394bab115eb180b5142
Zach W <zwheeler1026@gmail.com>
2022-11-09 20:28:28 -0500

    static = cow legs

--- src/main/java/frc/subsystems/Intake.java
+++ src/main/java/frc/subsystems/Intake.java
@@ -3,10 +3,20 @@ package frc.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Robot;
 import frc.robot.RobotMap;
 public class Intake extends SubsystemBase{
-    
+    static CANSparkMax intakeMotor;
+    DoubleSolenoid leftExtender;
+    DoubleSolenoid rightExtender;
+    boolean intakeExtended = false;
+    public Intake() {
+        intakeMotor = new CANSparkMax(RobotMap.INTAKE_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
+    }
+    public void startIntaking(double intakePercent){
+        intakeMotor.set(intakePercent);
+    }
+    public void stopIntaking() {
+        intakeMotor.set(0);
+    }
 }
