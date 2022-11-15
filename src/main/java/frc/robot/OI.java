a01de0ca9b7bb517eb8684483c20e6283b9e8885
Zach W <zwheeler1026@gmail.com>
2022-11-15 16:08:41 -0500

    Fixed Intake Maybe Idk

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,9 +1,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.commands.RunIntake;
 
 public class OI {
-
+    private JoystickButton manipXBtn, manipYBtn, manipABtn, manipBBtn;
 
     public XboxController driveController, manipController;
 
@@ -18,8 +21,11 @@ public class OI {
     private void initControllers() {
         driveController = new XboxController(0);
         manipController = new XboxController(1);
-
+        manipXBtn = new JoystickButton(manipController, XboxController.Button.kX.value);
+        manipXBtn.whenPressed(new RunIntake(true));
+        manipXBtn.whenReleased(new RunIntake(false));
     }
+
 
 
 }
