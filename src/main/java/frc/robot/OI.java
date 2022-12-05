8381d6e5d99b31ad08c5fefded8d5f866975f968
Zach W <zwheeler1026@gmail.com>
2022-12-05 18:36:49 -0500

    celeste

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -4,11 +4,14 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.commands.RunIntake;
+import frc.commands.*;
+import frc.subsystems.*;
 import frc.subsystems.Intake;
 
 public class OI {
     private JoystickButton manipXBtn, manipYBtn, manipABtn, manipBBtn, lBumperButton, rBumperButton;
     private Intake intake = new Intake();
+    private Shooter shooter = new Shooter();
     public XboxController driveController, manipController;
 
     public OI() {
@@ -25,5 +28,7 @@ public class OI {
         manipController = new XboxController(1);
         manipXBtn = new JoystickButton(manipController, XboxController.Button.kX.value);
         manipXBtn.whileHeld(new RunIntake(intake));
+        manipABtn = new JoystickButton(manipController, XboxController.Button.kA.value);
+        manipABtn.whileHeld(new Shoot(shooter));
     }
 }
