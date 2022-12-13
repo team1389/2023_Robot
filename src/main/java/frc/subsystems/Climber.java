d95022ba51147b35d4609bca115078b0bf34ebd4
Ria Gulati <riagulati111@gmail.com>
2022-12-12 19:20:23 -0500

    climber subsystem

--- src/main/java/frc/subsystems/Climber.java
+++ src/main/java/frc/subsystems/Climber.java
@@ -1,5 +1,72 @@
 package frc.subsystems;
 
-public class Climber {
-    
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public class Climber extends SubsystemBase {
+    private TalonSRX rightClimberMotor;
+    private TalonSRX leftClimberMotor;
+    private DoubleSolenoid leftSolenoid;
+    private DoubleSolenoid rightSolenoid;
+
+    enum Position {
+        Left,
+        Right
+    }
+
+    enum Action {
+        Extend,
+        Retract
+    }
+
+    public Climber() {
+        rightClimberMotor = new TalonSRX(RobotMap.RIGHT_CLIMB_MOTOR);
+        leftClimberMotor = new TalonSRX(RobotMap.LEFT_CLIMB_MOTOR);
+        leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, RobotMap.LEFT_CLIMBER_FORWARD_SOLENOID,
+                RobotMap.LEFT_CLIMBER_REVERSE_SOLENOID);
+        rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, RobotMap.RIGHT_CLIMBER_FORWARD_SOLENOID,
+                RobotMap.RIGHT_CLIMBER_REVERSE_SOLENOID);
+    }
+
+    public void controlArm(Position pos, Action act) {
+        TalonSRX motor;
+        switch (pos) {
+            case Left:
+                motor = leftClimberMotor;
+                break;
+            case Right:
+            default:
+                motor = rightClimberMotor;
+        }
+        switch (act) {
+            case Extend:
+                motor.set(TalonSRXControlMode.Position, 0.5);
+            case Retract:
+                motor.set(TalonSRXControlMode.Position, -0.5);
+        }
+    }
+
+    public void swingArm(Position pos, Action act) {
+        DoubleSolenoid solenoid;
+        switch (pos) {
+            case Left:
+                solenoid = leftSolenoid;
+                break;
+            case Right:
+            default:
+                solenoid = rightSolenoid;
+        }
+        switch (act) {
+            case Extend:
+                solenoid.set(Value.kForward);
+            case Retract:
+                solenoid.set(Value.kReverse);
+        }
+    }
 }
